a50d7a5720f9d4d0dc0668ca1fbac9fbf1c499c8
Furkan Doğu <furkandogu2018@gmail.com>
2024-01-09 14:29:10 +0300

    approuter düzenlendi

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,6 +4,12 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import PrivateRouter from "./PrivateRouter";
 import Dashboard from "../pages/Dashboard";
+import Home from "../pages/Home";
+import Purchases from "../pages/Purchases";
+import Sales from "../pages/Sales";
+import Firms from "../pages/Firms";
+import Brands from "../pages/Brands";
+import Products from "../pages/Products";
 
 const AppRouter = () => {
   return (
@@ -12,7 +18,14 @@ const AppRouter = () => {
         <Route path="/" element={<Login />} />
         <Route path="kayit" element={<Register />} />
         <Route path="stok" element={<PrivateRouter />}>
-          <Route path="" element={<Dashboard />} />
+          <Route path="" element={<Dashboard />}>
+            <Route index element={<Home />} />
+            <Route path="alimlar" element={<Purchases />} />
+            <Route path="satislar" element={<Sales />} />
+            <Route path="firmalar" element={<Firms />} />
+            <Route path="markalar" element={<Brands />} />
+            <Route path="urunler" element={<Products />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
