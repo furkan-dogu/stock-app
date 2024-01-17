61e51eb38d554776358e1bc370610a4a8cf51663
Furkan Doğu <furkandogu2018@gmail.com>
2024-01-17 16:45:01 +0300

    purchases bitti

--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -1,8 +1,70 @@
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useEffect, useState } from "react";
+import useStockCalls from "../service/useStockCalls";
+import PurchasesModal from "../components/PurchasesModal";
+import PurchasesTable from "../components/PurchasesTable";
+import { useSelector } from "react-redux";
+import TableSkeleton, { ErrorMsg, NoDataMsg } from "../components/DataFetchMsg";
 
 const Purchases = () => {
-  return (
-    <div>Purchases</div>
-  )
-}
+  const { purchases, loading, error } = useSelector((state) => state.stock);
 
-export default Purchases
+  const { getStocks } = useStockCalls();
+
+  const [data, setData] = useState({
+    firmId: "",
+    brandId: "",
+    productId: "",
+    quantity: "",
+    price: "",
+  });
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setData({
+      firmId: "",
+      brandId: "",
+      productId: "",
+      quantity: "",
+      price: "",
+    });
+  };
+
+  useEffect(() => {
+    getStocks("purchases");
+    getStocks("firms");
+    getStocks("brands");
+    getStocks("products");
+  }, []);
+
+  return (
+    <div>
+      <Typography variant="h4" color="error" mb={3}>
+        Purchases
+      </Typography>
+      <Button variant="contained" onClick={handleOpen} sx={{ mb: 3 }}>
+        New purchase
+      </Button>
+
+      <PurchasesModal
+        open={open}
+        handleClose={handleClose}
+        data={data}
+        setData={setData}
+      />
+
+      {error && <ErrorMsg />}
+
+      {loading && purchases.map((item) => <TableSkeleton />)}
+
+      {!error && !loading && !purchases.length && <NoDataMsg />}
+
+      {!loading && !error && purchases.length > 0 && <PurchasesTable handleOpen={handleOpen} setData={setData} data={data} />}
+    </div>
+  );
+};
+
+export default Purchases;
