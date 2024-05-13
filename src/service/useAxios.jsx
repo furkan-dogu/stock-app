7e17f224021793836f853c62b983ff1aee773a41
Furkan Doğu <furkandogu2018@gmail.com>
2024-05-13 22:24:39 +0300

    api değişikliği

--- src/service/useAxios.jsx
+++ src/service/useAxios.jsx
@@ -5,12 +5,14 @@ const useAxios = () => {
   const { token } = useSelector((state) => state.auth);
 
   const axiosWithToken = axios.create({
-    baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    // baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    baseURL: "https://stock-api-nodejs.vercel.app",
     headers: { Authorization: `Token ${token}` },
   });
 
   const axiosPublic = axios.create({
-    baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    // baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    baseURL: "https://stock-api-nodejs.vercel.app/",
   });
   
   return { axiosWithToken, axiosPublic };
